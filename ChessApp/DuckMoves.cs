628060d1988ca210eb9a71f80cc8ab22d6b997f5
Chris <ccw100asdf@gmail.com>
2022-10-05 09:10:26 +1100

    Added a move generator for the duck

---
+++ ChessApp/DuckMoves.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessApp
+{
+    public class DuckMoves
+    {
+        private static ulong GetMoves(Bitboard b)
+        {
+            return ~(b.WhitePieces | b.BlackPieces);
+        }
+        public static List<byte> DuckPositions(Bitboard b)
+        {
+            List<byte> result = new List<byte>();
+            var moves = GetMoves(b);
+            while (moves != 0)
+            {
+                byte lsb = (byte)(BitOperations.TrailingZeros(moves) - 1);
+                ulong bitpos = 1ul << lsb;
+                moves ^= bitpos; //remove this piece from the ulong of pieces
+                result.Add(lsb);
+            }
+            return result;
+        }
+    }
+}
